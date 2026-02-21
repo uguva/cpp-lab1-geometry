ec528ae0a94f8c63ad329a4b148a3e36c67f648b
uguva <itzsenkofox@gmail.com>
2026-02-22 04:59:51 +0700

    Добавлена задача 1d (Окружность) после Прямоугольника

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include <cmath> 
+#include <cmath>
 
 using namespace std;
 
 int main() {
+    // --- ЗАДАНИЕ 1a: ПРЯМОУГОЛЬНИК ---
     double a, b;
-    
+    cout << "--- Расчет Прямоугольника ---" << endl;
     cout << "Введите сторону a: ";
     cin >> a;
     cout << "Введите сторону b: ";
@@ -13,17 +14,31 @@ int main() {
 
     if (a <= 0 || b <= 0) {
         cout << "Ошибка: Стороны должны быть положительными!" << endl;
-        return 1;
+    } else {
+        cout << "Периметр: " << 2 * (a + b) << endl;
+        cout << "Площадь: " << a * b << endl;
+        cout << "Диагональ: " << sqrt(a * a + b * b) << endl;
     }
 
-    double perimeter = 2 * (a + b);
-    double area = a * b;
-    double diagonal = sqrt(a * a + b * b);
+    cout << endl << "--- ЗАДАНИЕ 1d: ОКРУЖНОСТЬ ---" << endl;
 
-    cout << "--- Результаты ---" << endl;
-    cout << "Периметр: " << perimeter << endl;
-    cout << "Площадь: " << area << endl;
-    cout << "Диагональ: " << diagonal << endl;
+    // --- ЗАДАНИЕ 1d: ОКРУЖНОСТЬ ---
+    const double PI = 3.1415926535;
+    double r, angle;
+
+    cout << "Введите радиус окружности: ";
+    cin >> r;
+    cout << "Введите угол сектора (в градусах): ";
+    cin >> angle;
+
+    if (r <= 0) {
+        cout << "Ошибка: Радиус должен быть положительным!" << endl;
+    } else {
+        cout << "Длина окружности: " << 2 * PI * r << endl;
+        cout << "Площадь круга: " << PI * r * r << endl;
+        cout << "Площадь сектора: " << (PI * r * r * angle) / 360.0 << endl;
+    }
 
     return 0;
 }
+
