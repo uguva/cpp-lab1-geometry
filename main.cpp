7a1eb494c77acd92e1205c94cc40aaf69b1665b1
uguva <itzsenkofox@gmail.com>
2026-02-22 04:18:04 +0700

    Реализовал расчеты прямоугольника (периметр, площадь, диагональ)

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath> 
+
+using namespace std;
+
+int main() {
+    double a, b;
+    
+    cout << "Введите сторону a: ";
+    cin >> a;
+    cout << "Введите сторону b: ";
+    cin >> b;
+
+    if (a <= 0 || b <= 0) {
+        cout << "Ошибка: Стороны должны быть положительными!" << endl;
+        return 1;
+    }
+
+    double perimeter = 2 * (a + b);
+    double area = a * b;
+    double diagonal = sqrt(a * a + b * b);
+
+    cout << "--- Результаты ---" << endl;
+    cout << "Периметр: " << perimeter << endl;
+    cout << "Площадь: " << area << endl;
+    cout << "Диагональ: " << diagonal << endl;
+
+    return 0;
+}
